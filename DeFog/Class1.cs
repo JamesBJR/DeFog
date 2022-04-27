23c3bf707579867613da541623ca99810a06c4b5
belowjobsrock <62730919+belowjobsrock@users.noreply.github.com>
2022-04-27 15:12:15 -0400

    Add project files.

---
+++ DeFog/Class1.cs
@@ -0,0 +1,44 @@
+﻿using MelonLoader;
+using System;
+using System.Linq;
+using UnityEngine;
+using Tropos;
+using Lirp;
+public class DeFog : MelonMod
+{
+
+    private GameObject refrenceObj;
+    private TroposCamera troposRefrence;
+    bool loadCheck = false;
+
+    public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+
+    {
+        if (loadCheck == true)
+        {
+            troposRefrence.renderFog = false;
+            troposRefrence.renderAerialPerspective = false;
+            troposRefrence.renderClouds = true;
+            
+        }
+        if (sceneName == "mountain01Logic")
+        {
+            if (loadCheck == false)
+            {
+                refrenceObj = GameObject.Find("VRCamera");
+                troposRefrence = refrenceObj.GetComponent<TroposCamera>();
+                troposRefrence.renderFog = false;
+                troposRefrence.renderAerialPerspective = false;
+                troposRefrence.renderClouds = true;
+                loadCheck = true;
+                
+            }
+            
+        }
+
+        return;
+    }
+
+
+
+}
